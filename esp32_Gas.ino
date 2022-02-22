0dcec17dd779a61eb4fe8993125b9ed1a8e1e8db
alifainur <58428727+alifainur@users.noreply.github.com>
2022-02-22 16:48:44 +0800

    Add files via upload

---
+++ esp32_Gas.ino
@@ -0,0 +1,29 @@
+const int buzzer = 23;
+const int gasSensor = 4;
+// Your threshold value
+const int sensorThres = 400;
+const int chan = 0;
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  pinMode(gasSensor, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int analogSensor = analogRead(gasSensor);
+
+  Serial.print("Pin 4: ");
+  Serial.println(analogSensor);
+  // Checks if it has reached the threshold value
+  if (analogSensor > sensorThres)
+  {
+    ledcAttachPin(buzzer,chan);
+    ledcWriteTone(chan, 300);
+  }
+  else
+  {
+    ledcDetachPin(buzzer);
+  }
+  delay(100);
+}
