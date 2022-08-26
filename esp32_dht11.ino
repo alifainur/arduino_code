730f9dd573193b3eb0845c8a49ef6f17a5d068ad
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-08-26 10:55:22 +0800

    Add files via upload

---
+++ esp32_dht11.ino
@@ -0,0 +1,23 @@
+#include "DHT.h"
+#define DHT11PIN 33 //jgn guna 34,35,36,39
+
+DHT dht(DHT11PIN, DHT11);
+void setup()
+{
+  
+  Serial.begin(115200);
+/* Start the DHT11 Sensor */
+  dht.begin();
+}
+
+void loop()
+{
+  float humi = dht.readHumidity();
+  float temp = dht.readTemperature();
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("ºC "); // press Alt + 0176 for ° Symbol
+  Serial.print("Humidity: ");
+  Serial.println(humi);
+  delay(1000);
+}
