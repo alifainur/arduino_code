29f9a1d24b50f19e9ea91a026d5b7dc5137dac20
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-09-21 10:43:35 +0800

    Update doubleLED_blink_Simple.ino

--- doubleLED_blink_Simple.ino
+++ doubleLED_blink_Simple.ino
@@ -7,12 +7,12 @@ pinMode(led2,OUTPUT);
 }
 
 void loop() {
-digitalWrite(led1,!digitalRead(led1));
+digitalWrite(led1,HIGH);
 delay(2000);
-digitalWrite(led1,!digitalRead(led1));
+digitalWrite(led1,LOW);
 delay(2000);
-digitalWrite(led2,!digitalRead(led2));
+digitalWrite(led2,HIGH);
 delay(200);
-digitalWrite(led2,!digitalRead(led2));
+digitalWrite(led2,LOW);
 delay(200);
 }
