7cb56c46ba9a2fdd5fa8f4b5889d714260888a93
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-09-21 11:31:02 +0800

    Add files via upload

---
+++ esp32_FreeRTOS_loop.ino
@@ -0,0 +1,25 @@
+const int led1 = 19;
+const int led2 = 23;
+
+TaskHandle_t Task1_handle;
+
+void task1(void * parameters){
+  for(;;){
+    Serial.printf("led1 Core#%i \n", xPortGetCoreID());
+    digitalWrite(led1, !digitalRead(led1));
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  xTaskCreatePinnedToCore(task1, "Task 1", 5000, NULL, 1, &Task1_handle, 0);
+}
+
+void loop() {
+  Serial.printf("led2 Core#%i \n", xPortGetCoreID());
+  digitalWrite(led2, !digitalRead(led2));
+  delay(1000);
+}
