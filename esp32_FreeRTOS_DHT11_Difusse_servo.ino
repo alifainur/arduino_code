d61ab6f65966508e9962388c36f99d4fd1e15ba0
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-09-23 10:53:09 +0800

    Add files via upload

---
+++ esp32_FreeRTOS_DHT11_Difusse_servo.ino
@@ -0,0 +1,44 @@
+#include <ESP32Servo.h>
+#include <DHT.h>
+DHT dht(32, DHT11);
+Servo weeekly;
+
+const int diffuseSensor = 13;
+int sensorState = 0;
+
+TaskHandle_t Task1;
+
+void loop2( void * parameter ){
+  for (;;) {
+    delay(1000);
+    float humi = dht.readHumidity();
+    float temp = dht.readTemperature();
+    if (isnan(humi) || isnan(temp)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+    Serial.printf(("          Temperature: %.2f°C \t Humidity: %.2f%% \n"),temp,humi);
+  }
+}
+
+void setup() {
+  weeekly.attach(12,500,2500);
+  pinMode(diffuseSensor,INPUT);
+  Serial.begin(115200);
+  dht.begin();
+
+  xTaskCreatePinnedToCore(loop2, "dht11", 5000, NULL, 1, &Task1, 0);
+}
+
+void loop() {
+  sensorState = digitalRead(diffuseSensor);
+  if (sensorState == LOW){
+    weeekly.write(0);
+    Serial.println("Gate Open");
+    delay(1000);
+  }else{
+    weeekly.write(180);
+    Serial.println("Gate Close");
+  }
+  delay(50);
+}
