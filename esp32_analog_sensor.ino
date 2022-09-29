de100b852219090c7149f7e7224aeb44bb35f0ca
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-09-29 11:55:27 +0800

    Add files via upload

--- esp32_analog_sensor.ino
+++ esp32_analog_sensor.ino
@@ -1,14 +1,29 @@
 const int analog_ip = 4;
+const int led1 = 18;
+const int led2 = 19;
 int inputValue = 0;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
 void loop() {
   inputValue = analogRead(analog_ip);
-  Serial.println(inputValue);
-  delay(1000);
+Serial.println(inputValue);
+  if (inputValue >= 0 && inputValue <= 500) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+  }
+  if ((inputValue >= 501) && (inputValue <= 1199)) {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+  }
+  if ((inputValue >= 1200) && (inputValue <= 1700)) {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+  }
+
+  delay(15);
 }
-
-
