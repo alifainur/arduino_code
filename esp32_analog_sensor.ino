4358d1148c10fbb1e546c892d6be4bef706fce6a
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-09-29 11:09:19 +0800

    Add files via upload

---
+++ esp32_analog_sensor.ino
@@ -0,0 +1,14 @@
+const int analog_ip = 4;
+int inputValue = 0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  inputValue = analogRead(analog_ip);
+  Serial.println(inputValue);
+  delay(1000);
+}
+
+
