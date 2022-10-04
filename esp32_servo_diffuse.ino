5a41c345dbf4ae297389fc86b0846e26ab965458
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-10-04 10:46:10 +0800

    Update esp32_servo_diffuse.ino

--- esp32_servo_diffuse.ino
+++ esp32_servo_diffuse.ino
@@ -1,22 +1,23 @@
 #include <ESP32Servo.h>
-Servo weeekly;
+Servo weeekly; // menamakan servo objek dengan nama 'weeekly'
 
-const int diffuseSensor = 13;
+const int diffuseSensor = 13; // variable const = read only
 int sensorState = 0;
 
 void setup() {
-  weeekly.attach(12,500,2500);
+  weeekly.attach(12,500,2500); //12=pin, 500=min, 2500=max
   pinMode(diffuseSensor,INPUT);
   Serial.begin(115200);
 }
 void loop() {
-  sensorState = digitalRead(diffuseSensor);
-  if (sensorState == LOW){
-    weeekly.write(0);
+  sensorState = digitalRead(diffuseSensor); // bacaan dari diffuseSensor dimasukkan ke sensorState
+  
+  if (sensorState == LOW){ // buat perbandingan jika sama dengan LOW
+    weeekly.write(0); // suruh servo gerak 0°
     Serial.println("Gate Open");
     delay(1000);
   }else{
-    weeekly.write(180);
+    weeekly.write(180); // suruh servo gerak 180°
     Serial.println("Gate Close");
   }
   delay(50);
