dbcde812212b780febcd6145fbe26ba56aa6d6d2
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-10-04 10:38:24 +0800

    Update esp32_servo_basic.ino

--- esp32_servo_basic.ino
+++ esp32_servo_basic.ino
@@ -1,13 +1,13 @@
 #include <ESP32Servo.h>
-Servo weeekly;
+Servo weeekly; // buat servo objek dengan nama 'weeekly'
 
 void setup() {
-  weeekly.attach(12,500,2500);
+  weeekly.attach(12,500,2500); // 12=pin, 500=min, 2500=max
 }
 
 void loop() {
-  weeekly.write(0);
+  weeekly.write(0); // suruh servo gerak 0°
   delay(1000);
-  weeekly.write(180);
+  weeekly.write(180); // suruh servo gerak 180°
   delay(1000);
 }
