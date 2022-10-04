8b8f9c8312ff8dfeaf75d98d90dd45b83f41a9e2
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-10-04 11:08:48 +0800

    Add files via upload

---
+++ esp32_analogRead_Write_pot_led.ino
@@ -0,0 +1,20 @@
+const int potPin = 4;
+int inputVal = 0;
+const int ledPin = 23;
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
+
+void setup() {
+ Serial.begin(115200);
+   ledcSetup(ledChannel, freq, resolution);
+   ledcAttachPin(ledPin, ledChannel);
+ delay(1000);
+}
+
+void loop() {
+ inputVal = analogRead (potPin);
+ Serial.println(inputVal);
+ ledcWrite(ledChannel, inputVal/4);
+ delay(500);
+}
