5256900bd04e0660b24fb717b99717585b14296b
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-10-04 12:49:45 +0800

    Add files via upload

---
+++ esp32_serialRead_node-red.ino
@@ -0,0 +1,28 @@
+#include <DHT.h>
+#define DHT11PIN 32 //jgn guna 34,35,36,39
+
+DHT dht(DHT11PIN, DHT11);
+
+char temp[10];
+char humi[10];
+
+void setup()
+{
+  
+  Serial.begin(115200);
+/* Start the DHT11 Sensor */
+  dht.begin();
+}
+
+void loop()
+{
+  float h = dht.readHumidity();
+  dtostrf(h, 4, 2, humi);
+  float t = dht.readTemperature();
+  dtostrf(t, 4, 2, temp);
+
+  Serial.print(temp);
+  Serial.print("\n");
+  Serial.println(humi);
+  delay(1000);
+}
