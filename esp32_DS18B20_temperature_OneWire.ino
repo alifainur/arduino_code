341e6f2c0fdf77bae43013dccce3c2961c6393f8
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-10-04 11:50:07 +0800

    Add files via upload

---
+++ esp32_DS18B20_temperature_OneWire.ino
@@ -0,0 +1,21 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+const int tempSensor = 23;  //GPIO used for sensor
+OneWire oneWire(tempSensor); //Set Communication
+DallasTemperature sensors(&oneWire); //Pass OneWire to Dallas
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures(); 
+  float tempCelsius = sensors.getTempCByIndex(0);
+  float tempFahrenheit = sensors.getTempFByIndex(0);
+  Serial.print(tempCelsius);
+  Serial.print("ºC    "); //Alt + 0176
+  Serial.print(tempFahrenheit);
+  Serial.println("ºF"); //Alt + 0176
+  delay(2000);
+}
