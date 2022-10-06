18a024d3641c9a5724c38d5ebc7cb8c3f076f60c
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-10-07 05:37:16 +0800

    Add files via upload

---
+++ esp32_button_led_holding.ino
@@ -0,0 +1,27 @@
+const int ledPin = 23;
+int ledState = 0;
+
+const int buttonPin = 4;
+int buttonNew;
+int buttonOld = 1;
+
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  pinMode(buttonPin,INPUT);
+}
+
+void loop() {
+  buttonNew=digitalRead(buttonPin);
+  if(buttonOld==0 && buttonNew==1){
+if(ledState==0){
+digitalWrite(ledPin,HIGH);
+ledState=1;
+}    
+else{
+  digitalWrite(ledPin,LOW);
+  ledState=0;
+}
+}
+buttonOld=buttonNew;
+delay(100);
+}
