1c9acfaa2ec5efce16a2dc08b5b7d67fde422374
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2024-04-29 09:29:50 +0800

    Update mqtt_2relay.ino

--- mqtt_2relay.ino
+++ mqtt_2relay.ino
@@ -14,7 +14,7 @@ const int relay1 = 21;
 const int relay2 = 22;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
