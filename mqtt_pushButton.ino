bf8336423233224e007cb277182751e8d8405158
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2024-04-29 10:09:09 +0800

    Update mqtt_pushButton.ino

--- mqtt_pushButton.ino
+++ mqtt_pushButton.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const int buttonPin = 23;
+const int buttonPin = 26;
 int buttonState = 0;
 const char* ssid = "yourSSID";
 const char* password = "yourPASSWORD";
@@ -64,7 +64,7 @@ void reconnect() {
 } //end reconnect()
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
