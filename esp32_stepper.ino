b61ba5451b66106f0c53fc8b729aad54193414b7
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2024-04-29 09:53:00 +0800

    Update esp32_stepper.ino

--- esp32_stepper.ino
+++ esp32_stepper.ino
@@ -3,10 +3,10 @@
 const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
 
 // ULN2003 Motor Driver Pins
-#define IN1 18
-#define IN2 19
-#define IN3 23
-#define IN4 5
+#define IN1 5
+#define IN2 17
+#define IN3 32
+#define IN4 35
 
 // initialize the stepper library
 Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
@@ -15,7 +15,7 @@ void setup() {
   // set the speed at 5 rpm
   myStepper.setSpeed(5);
   // initialize the serial port
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop() {
