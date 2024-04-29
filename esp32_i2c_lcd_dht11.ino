6e0e8c0d7f93149ffd1bec73e7c46d33ab7b8744
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2024-04-29 08:52:56 +0800

    Update esp32_i2c_lcd_dht11.ino

--- esp32_i2c_lcd_dht11.ino
+++ esp32_i2c_lcd_dht11.ino
@@ -1,12 +1,12 @@
 #include "DHT.h"
-DHT dht(19, DHT11);
+DHT dht(33, DHT11);
 #include <LiquidCrystal_I2C.h>
 int lcdColumns = 16;
 int lcdRows = 2;
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
   dht.begin();
   lcd.init();                   
   lcd.backlight();
