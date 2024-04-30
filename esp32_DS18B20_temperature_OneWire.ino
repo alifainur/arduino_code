eb864fc5f62131a6068d93aa4fa3291950f6b108
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2024-04-30 15:29:44 +0800

    Update esp32_DS18B20_temperature_OneWire.ino

--- esp32_DS18B20_temperature_OneWire.ino
+++ esp32_DS18B20_temperature_OneWire.ino
@@ -5,7 +5,7 @@ OneWire oneWire(tempSensor); //Set Communication
 DallasTemperature sensors(&oneWire); //Pass OneWire to Dallas
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   sensors.begin();
 }
 
