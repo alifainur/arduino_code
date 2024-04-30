c26929b35464ca7bf9955bcec68286e5366d2116
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2024-04-30 15:36:10 +0800

    Update esp32_WiFi_keepConnect.ino

--- esp32_WiFi_keepConnect.ino
+++ esp32_WiFi_keepConnect.ino
@@ -27,7 +27,7 @@ void keepWiFiAlive(void * parameters){
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   connectToWiFi();
   xTaskCreatePinnedToCore(keepWiFiAlive, "Keep WiFi Alive", 5000, NULL, 1, NULL, CONFIG_ARDUINO_RUNNING_CORE);
 }
