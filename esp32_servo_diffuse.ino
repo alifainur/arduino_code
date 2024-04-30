b6e9ca620ea7bb692c38d2fff9e8e59fc1336a92
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2024-04-30 15:54:51 +0800

    Update esp32_servo_diffuse.ino

--- esp32_servo_diffuse.ino
+++ esp32_servo_diffuse.ino
@@ -7,7 +7,7 @@ int sensorState = 0;
 void setup() {
   weeekly.attach(12,500,2500); //12=pin, 500=min, 2500=max
   pinMode(diffuseSensor,INPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 void loop() {
   sensorState = digitalRead(diffuseSensor); // bacaan dari diffuseSensor dimasukkan ke sensorState
