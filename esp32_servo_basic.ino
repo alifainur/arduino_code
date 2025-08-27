0be4d0d09750e1a5e1c4cd09442662139f5513c0
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2025-08-28 07:24:26 +0800

    Update esp32_servo_basic.ino

--- esp32_servo_basic.ino
+++ esp32_servo_basic.ino
@@ -1,19 +1,21 @@
 #include <ESP32Servo.h>
-Servo weeekly; // buat servo objek dengan nama 'weeekly'
+Servo weeekly;  // menamakan servo sebagai 'weeekly'
 
-void setup() {
+void setup() 
+{
   ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	weeekly.setPeriodHertz(50);
-  weeekly.attach(26,500,2500); // 26=pin, 500=min(500us), 2500=max(2500us)
-  // PWM GPIO pin yang digalakkan di ESP32 termasuk pin 2,4,12-19,21-23,25-27,32-33 
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  weeekly.setPeriodHertz(50);
+  weeekly.attach(26, 500, 2500);  // 26=pin, 500=minimum(500us), 2500=maximum(2500us)
+  // Pin GPIO PWM yang digalakkan di ESP32 adalah pin 2,4,12-19,21-23,25-27,32-33
 }
 
-void loop() {
-  weeekly.write(0); // suruh servo gerak 0°
+void loop() 
+{
+  weeekly.write(0);  // kedudukan servo 0°
   delay(1500);
-  weeekly.write(180); // suruh servo gerak 180°
+  weeekly.write(180);  // kedudukan servo 180°
   delay(1500);
 }
