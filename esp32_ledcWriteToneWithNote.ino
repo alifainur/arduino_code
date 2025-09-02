cc0ef7c9f9db0f5c2dffb7776a88e48b84f63f1c
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2025-09-02 19:17:08 +0800

    Update esp32_ledcWriteToneWithNote.ino

--- esp32_ledcWriteToneWithNote.ino
+++ esp32_ledcWriteToneWithNote.ino
@@ -1,36 +1,36 @@
-const int pin = 26;
+const int buzzerPin = 16;
 const int freq = 5000;
 const int resolution = 8;
 
 void setup() {
-  ledcAttach(pin, freq, resolution);
+  ledcAttach(buzzerPin, freq, resolution);
 }
 
 void loop() {
-    ledcWriteNote(pin, NOTE_E, 5);
-    delay(800);
-    ledcWriteNote(pin, NOTE_D, 5);   
-    delay(400);
-    ledcWriteNote(pin, NOTE_Fs, 4);
-    delay(400);
-    ledcWriteNote(pin, NOTE_Gs, 4);   
-    delay(800);
-    ledcWriteNote(pin, NOTE_Cs, 5);
-    delay(800);
-    ledcWriteNote(pin, NOTE_B, 4);   
-    delay(400);
-    ledcWriteNote(pin, NOTE_D, 4);
-    delay(400);
-    ledcWriteNote(pin, NOTE_E, 4);   
-    delay(800);
-    ledcWriteNote(pin, NOTE_B, 4);
-    delay(800);
-    ledcWriteNote(pin, NOTE_A, 4);   
-    delay(400);
-    ledcWriteNote(pin, NOTE_Cs, 4);
-    delay(400);
-    ledcWriteNote(pin, NOTE_E, 4);   
-    delay(200);
-    ledcWriteNote(pin, NOTE_A, 4);   
+    ledcWriteNote(buzzerPin, NOTE_E, 5);
+    delay(100);
+    ledcWriteNote(buzzerPin, NOTE_D, 5);   
+    delay(100);
+    ledcWriteNote(buzzerPin, NOTE_Fs, 4);
+    delay(300);
+    ledcWriteNote(buzzerPin, NOTE_Gs, 4);   
+    delay(300);
+    ledcWriteNote(buzzerPin, NOTE_Cs, 5);
+    delay(100);
+    ledcWriteNote(buzzerPin, NOTE_B, 4);   
+    delay(100);
+    ledcWriteNote(buzzerPin, NOTE_D, 4);
+    delay(300);
+    ledcWriteNote(buzzerPin, NOTE_E, 4);   
+    delay(300);
+    ledcWriteNote(buzzerPin, NOTE_B, 4);
+    delay(100);
+    ledcWriteNote(buzzerPin, NOTE_A, 4);   
+    delay(100);
+    ledcWriteNote(buzzerPin, NOTE_Cs, 4);
+    delay(300);
+    ledcWriteNote(buzzerPin, NOTE_E, 4);   
+    delay(300);
+    ledcWriteNote(buzzerPin, NOTE_A, 4);   
     delay(800);
 }
